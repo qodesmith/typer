5dfddc133a9bc3e7e9cac4e7cd6a5132d35ae6f7
Qodesmith <theqodesmith@gmail.com>
2021-02-06 11:07:08 -0500

    Few tweaks to the helper fxn.

--- buildStylesHelper.js
+++ buildStylesHelper.js
@@ -4,20 +4,23 @@ const path = require('path')
 const filePath = path.resolve('./typer.css')
 const css = fs.readFileSync(filePath, 'utf8').split('\n')[0]
 const fileContent = `/*
-  This file is meant to be imported into environments that only allow JavaScript
+  This function is meant to be used in environments that only allow JavaScript
   modules to be imported but not assets like CSS. Typer needs the CSS to work.
-  You almost certainly don't need to use this file.
+
+  You almost certainly don't need to use this function.
 */
 
 function applyStylesToDom() {
-  const css = '${css}'
+  const css =
+    '${css}'
   const id = '__typer-js-style-tag'
 
   // See if the document already has the styles we need.
   const headLinks = [...document.head.querySelectorAll('link')]
-  const dontAppend = !!headLinks.find(link => link.getAttribute('href').endsWith('typer.css'))
+  const dontAppend = !!headLinks.find(link =>
+    link.getAttribute('href').endsWith('typer.css')
+  )
   if (dontAppend || document.getElementById(id) != null) return
-
 
   const style = document.createElement('style')
   style.setAttribute('id', id)
