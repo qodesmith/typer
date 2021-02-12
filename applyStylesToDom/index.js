d51e3a01099c51a4f375bf71a0084018fd2f316c
Qodesmith <theqodesmith@gmail.com>
2021-02-12 07:42:05 -0500

    Better cursor alignment

--- applyStylesToDom/index.js
+++ applyStylesToDom/index.js
@@ -7,7 +7,7 @@
 
 function applyStylesToDom() {
   const css =
-    `.typer:after{content:"";display:inline-block;vertical-align:middle;width:.1em;height:1em;background-color:currentColor;margin-left:.1em}.cursor-block:after{width:.5em}.cursor-soft:after{-webkit-animation:softblink .7s infinite;animation:softblink .7s infinite}.cursor-hard:after{-webkit-animation:hardblink .7s infinite;animation:hardblink .7s infinite}.no-cursor:after{content:none}.white-space{white-space:pre-wrap}@-webkit-keyframes softblink{0%{opacity:1}50%{opacity:0}to{opacity:1}}@keyframes softblink{0%{opacity:1}50%{opacity:0}to{opacity:1}}@-webkit-keyframes hardblink{50%,to{visibility:hidden}}@keyframes hardblink{50%,to{visibility:hidden}}`
+    `[data-typer-child]{position:relative}.typer:after{content:"";display:inline-block;width:.1em;height:2ch;background-color:currentColor;margin-left:.1em;position:absolute;top:50%;-webkit-transform:translateY(-50%);transform:translateY(-50%)}.cursor-block:after{width:.5em}.cursor-soft:after{-webkit-animation:softblink .7s infinite;animation:softblink .7s infinite}.cursor-hard:after{-webkit-animation:hardblink .7s infinite;animation:hardblink .7s infinite}.no-cursor:after{content:none}.white-space{white-space:pre-wrap}@-webkit-keyframes softblink{0%{opacity:1}50%{opacity:0}to{opacity:1}}@keyframes softblink{0%{opacity:1}50%{opacity:0}to{opacity:1}}@-webkit-keyframes hardblink{50%,to{visibility:hidden}}@keyframes hardblink{50%,to{visibility:hidden}}`
   const id = '__typer-js-style-tag'
 
   // See if the document already has the styles we need.
--- sandboxApp/sandbox.js
+++ sandboxApp/sandbox.js
@@ -1,7 +1,13 @@
-Array.from(document.querySelectorAll('section')).forEach(section => {
-  Array.from(section.querySelectorAll('div')).forEach((div, i) => {
+// These styles are declared in `index.html` inside the styles in the header.
+const fontFaces = ['monospace', 'sans-serif', 'serif', 'cursive', 'fantasy']
+
+Array.from(document.querySelectorAll('section')).forEach((section, i) => {
+  const fontFace = fontFaces[i]
+
+  Array.from(section.querySelectorAll('div')).forEach((div, j) => {
     typer(div, 5)
-      .cursor({block: i === 0})
-      .line('JavaScript')
+      .cursor({block: j === 0})
+      .line(`JavaScript - ${fontFace}`)
+      .continue('<span style="background: pink"> this is a span</span>')
   })
 })
